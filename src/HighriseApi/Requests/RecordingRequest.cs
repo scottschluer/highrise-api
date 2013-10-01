5f21ddf9dd29962dc03bab3b904ad175de0d5eef
Pieter van der Westhuizen <pieter@csoft.co.za>
2013-10-01 14:57:38 +0200

    Implemented support for Recordings

---
+++ src/HighriseApi/Requests/RecordingRequest.cs
@@ -0,0 +1,28 @@
+﻿using HighriseApi.Interfaces;
+using HighriseApi.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HighriseApi.Requests
+{
+    public class RecordingRequest : RequestBase,IRecordingRequest
+    {
+        public RecordingRequest(IRestClient client) : base(client) { }
+
+        public IEnumerable<Recording> Get()
+        {
+            var response = _client.Execute<List<Recording>>(new RestRequest("recordings.xml", Method.GET));
+            return response.Data;
+        }
+
+        public IEnumerable<Recording> Get(DateTime startDate)
+        {
+            var url = String.Format("recordings.xml?since={0}", startDate.ToString("yyyyMMddHHmmss"));
+            var response = _client.Execute<List<Recording>>(new RestRequest(url, Method.GET));
+            return response.Data;
+        }
+    }
+}
